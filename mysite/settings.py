f08cb60b6428581515097eb8e221971e3a7b35a5
lingxt <lxt9601@gmail.com>
2021-05-31 11:34:16 +0900

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'github_actions',
-        'USER': 'radorado',
-        'PASSWORD': 'radorado',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
